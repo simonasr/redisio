fa499dbd26d802f6fda757ff9e064e3ed88b55b9
Simonas Rupsys <simonas.rupsys@vinted.com>
2024-01-02 11:20:14 +0200

    r7

--- attributes/default.rb
+++ attributes/default.rb
@@ -46,7 +46,7 @@ default['redisio']['version'] = if node['redisio']['package_install']
                                   nil
                                 else
                                   # force version for tarball
-                                  '6.2.14'
+                                  '7.2.3'
                                 end
 
 # Custom installation directory
